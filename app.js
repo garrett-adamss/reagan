19396bcd0081d7c749e5abd810605ce8cddfa060
Garrett <garrettadams27@gmail.com>
2022-12-01 16:52:33 -0700

    fixing

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('hello b0ss')
+// Parallax Scroll ID's
+let thumbnail = document.getElementById('thumbnail')
+
+//Mobile var
+let mobileWidth = window.innerWidth
+
+//Parallax Scroll if statement
+if(mobileWidth >= 768){ //Mobile check
+
+  window.addEventListener('wheel', function(){
+    let scroll = window.scrollY; //px
+    // console.log('[horizontal]', horizontal)
+
+    let viewValue = (100 * (window.scrollY)) / window.innerHeight; // base value
+
+    console.log("[window.scrollY] :", window.scrollY ,`
+    [window.innerHeight] :`, window.innerHeight, `
+    [viewValue in vh] :`,  viewValue)
+ 
+    //TEST
+    // if(view)
+
+  })
+}
