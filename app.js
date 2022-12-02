f2164c3b9211b54b4216cfa95e45e176a7cf383d
Garrett <garrettadams27@gmail.com>
2022-12-01 20:39:58 -0700

    some mobile

--- app.js
+++ app.js
@@ -2,6 +2,17 @@
 let thumbnail = document.getElementById('thumbnail')
 let title = document.getElementById('title')
 
+let aboutMe = document.getElementById('about-me')
+let projectOne = document.getElementById('project-one')
+let projectTwo = document.getElementById('project-two')
+let projectThree = document.getElementById('project-three')
+let projectFour = document.getElementById('project-four')
+let projectFive = document.getElementById('project-five')
+let projectSix = document.getElementById('project-six')
+let projectSeven = document.getElementById('project-seven')
+let contact = document.getElementById('contact')
+
+
 //Mobile Var
 let mobileWidth = window.innerWidth
 
@@ -22,16 +33,29 @@ outerWrapper.onscroll = function() {
     // @ts-ignore
     title.style.left = (widthValue) + 'vw'
     // @ts-ignore                                   BOSS THAT LOOKS FIRE GOING GREAT.
-    title.style.opacity = 1 - (widthValue * 0.012)
+    title.style.opacity = 1 - (widthValue * 0.013)
   } else {
   // @ts-ignore
   title.style.left = ((widthValue * .1)+ 75) + 'vw';
   }
 
+  //!SECTION PROJECTS
 
+    //Slide One
+  if(widthValue >= 100){
+      // @ts-ignore
+    aboutMe.style.left = (-(widthValue * .3) + 70) + 'vw';
+    // @ts-ignore
+    projectOne.style.left = (-(widthValue * .15) + 75 ) + 'vw';
+    // @ts-ignore
+    projectTwo.style.left = (-(widthValue * .35) + 158) + 'vw';
+    // @ts-ignore
+    projectThree.style.left = (-(widthValue * .15) + 150) + 'vw';
+    // @ts-ignore
+    projectFour.style.left = (-(widthValue * .3) + 230) + 'vw';
+    // @ts-ignore
+    projectFive.style.left = (-(widthValue * .15) + 215) + 'vw';
   
-
-
-
+  }
 
 }
